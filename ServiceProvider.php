ed32cec96e6afac65c838ef36d938ac24b0e957f
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-06-12 14:41:36 +0000

    Initial commit of local changes online

---
+++ ServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drewlabs\Packages\Database;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Drewlabs\Core\Database\NoSql\DatabaseManager;
+use Drewlabs\Packages\Database\Contracts\TransactionUtils;
+use Drewlabs\Packages\Database\DataTransactionUtils;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        $this->bindings();
+    }
+
+    protected function bindings()
+    {
+        // Solve issue related to version of MySQL older than the 5.7.7 release or MariaDB older than the 10.2.2.
+        $this->app['db']->connection()->getSchemaBuilder()->defaultStringLength(255);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(TransactionUtils::class, function ($app) {
+            return new DataTransactionUtils($app);
+        });
+        // Register Nosql providers bindings
+        $this->noSqlBindings();
+    }
+
+    /**
+     * Binding for Nosql Data providers
+     *
+     * @return void
+     */
+    protected function noSqlBindings()
+    {
+        // Binding for Noqsl Database Manager
+        $this->app->bind('nosqlDb', function () {
+            new DatabaseManager(config('database.nosql_driver', 'mongo'));
+        });
+    }
+}
