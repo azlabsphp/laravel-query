a2ecdb6ffce045b732059fa6887f3e987f2f2854
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2020-06-21 10:16:25 +0000

    Apply array like query parameters in Illuminate base model query parameters parser

--- src/Traits/IlluminateBaseModel.php
+++ src/Traits/IlluminateBaseModel.php
@@ -31,6 +31,10 @@ trait IlluminateBaseModel
                     $column = $exploded[1];
                     if (method_exists($this, $relation) && !is_null($column)) {
                         $filters['whereHas'][] = array($relation, function ($query) use ($value, $column) {
+                            if (is_array($value)) {
+                                $query->whereIn($column, $value);
+                                return;
+                            }
                             $operator = is_numeric($value) || is_bool($value) ? '=' : 'like';
                             $value = is_numeric($value) ? $value : '%' . $value . '%';
                             $query->where($column, $operator, $value);
