ff88969721a1733d41519e1d0f5b4f7a791f2022
azandrew-sidoine <azandrewdevelopper@gmail.com>
2025-06-28 09:46:29 +0000

    source code cleanup

--- tests/Unit/CountDuplicatesAggregationQueryTest.php
+++ tests/Unit/CountDuplicatesAggregationQueryTest.php
@@ -21,17 +21,44 @@ class CountDuplicatesAggregationQueryTest extends TestCase
 
     public function test_query_filters_count_by()
     {
+
         $filters = new QueryFilters([
-            'aggregate' => ['addCount' => [['age', 'and(sex, = , M)', 'age_count']]]
+            'aggregate' => [
+                'count' => [
+                    ['code', ['and' => [['published', '=', 1]]], 'published_product_count'],
+                    ['code', ['and' => [['published', '=', 0]]], 'not_published_product_count']
+                ],
+                'sum' => [
+                    [['rating', 'code'], ['and' => [['published', '=', 1]]], 'sum_published_rating'],
+                    [['rating', 'code'], ['and' => [['published', '=', 0]]], 'sum_not_published_rating'],
+                ],
+                'min' => [
+                    [['rating', 'code'], ['and' => [['published', '=', 1]]], 'min_published_rating'],
+                    [['rating', 'code'], ['and' => [['published', '=', 0]]], 'min_not_published_rating'],
+                ],
+                'max' => [
+                    [['rating', 'code'], ['and' => [['published', '=', 1]]], 'max_published_rating'],
+                    [['rating', 'code'], ['and' => [['published', '=', 0]]], 'max_not_published_rating'],
+                ]
+            ],
         ]);
 
         // Act
-        $result = $filters->apply(Person::query());
-        $match = $result->get()->where(function($item) {
-            return $item->email === 'benjaminpayaro@gmail.com';
+        $result = $filters->apply(Product::query());
+
+        
+        $value = $result->get()->where(static function ($item) {
+            return 'P0001' === $item->code;
         })->first();
 
         // Assert
-        $this->assertEquals(2, $match->age_count);
+        $this->assertSame(2, $value->published_product_count);
+        $this->assertSame(2, $value->not_published_product_count);
+        $this->assertSame(7, $value->sum_published_rating);
+        $this->assertSame(3, $value->sum_not_published_rating);
+        $this->assertSame(3, $value->min_published_rating);
+        $this->assertSame(1, $value->min_not_published_rating);
+        $this->assertSame(4, $value->max_published_rating);
+        $this->assertSame(2, $value->max_not_published_rating);
     }
 }
